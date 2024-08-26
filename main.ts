95adb12266cea3dc16816c3298857e96794ac8ef
Matheus de Souza Guedes <130612760+MatheusGuedes0@users.noreply.github.com>
2024-08-26 16:29:27 -0300

    Alteração no código com sortNumbers

--- main.ts
+++ main.ts
@@ -1,10 +1,36 @@
-radio.onReceivedString(function (receivedString) {
-    resposta = receivedString
-    if (resposta == "Sim") {
-        basic.showIcon(IconNames.Yes)
+let numero = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    numero = randint(1, 6)
+    if (numero == 1) {
+        led.plot(2, 2)
+    } else if (numero == 2) {
+        led.plot(0, 0)
+        led.plot(4, 4)
+    } else if (numero == 3) {
+        led.plot(0, 0)
+        led.plot(4, 4)
+        led.plot(2, 2)
+    } else if (numero == 4) {
+        led.plot(0, 0)
+        led.plot(4, 4)
+        led.plot(4, 0)
+        led.plot(0, 4)
+    } else if (numero == 5) {
+        led.plot(0, 0)
+        led.plot(4, 4)
+        led.plot(4, 0)
+        led.plot(0, 4)
+        led.plot(2, 2)
+    } else if (numero == 6) {
+        led.plot(0, 0)
+        led.plot(0, 4)
+        led.plot(0, 2)
+        led.plot(4, 2)
+        led.plot(4, 0)
+        led.plot(0, 4)
+        led.plot(4, 4)
     } else {
         basic.showIcon(IconNames.No)
     }
 })
-let resposta = ""
-radio.setGroup(4)
